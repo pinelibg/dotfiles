527f11ae416f8ecf81b0a0b4b137fca49b742494
pine <pinelibg@outlook.jp>
2017-05-17 00:15:32 +0900

    More ls aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,6 +1,11 @@
 alias ls='ls --color=auto'
-alias ll="ls -l"
-alias du="du -h"
-alias df="df -h"
-alias grep="grep --color=auto"
+alias ll='ls -l'
+alias la='ls -a'
+alias lla='ls -la'
+alias du='du -h'
+alias df='df -h'
+alias grep='grep --color=auto'
+
+# enable alias in sudo
+alias sudo='sudo '
 
