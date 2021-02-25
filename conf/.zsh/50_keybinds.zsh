814a94359102c2f4fa97e37362558461aaac4ffc
pinelibg <pinelibg@outlook.jp>
2021-02-25 11:22:26 +0900

    Add keybind to save a partially written command

--- conf/.zsh/50_keybinds.zsh
+++ conf/.zsh/50_keybinds.zsh
@@ -5,3 +5,9 @@ bindkey -e
 bindkey '^P' history-beginning-search-backward
 bindkey '^N' history-beginning-search-forward
 
+# Ctrl+Q: Save a partially written commands and resume after the next command
+# or make all lines editable when writing a multi-line command
+bindkey '^Q' push-line-or-edit
+bindkey '^[Q' push-line-or-edit
+bindkey '^[q' push-line-or-edit
+
