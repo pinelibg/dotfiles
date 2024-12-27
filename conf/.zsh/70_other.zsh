f89899352e4a868baeaa82786a905a8145e611e1
pinelibg <pinelibg@outlook.jp>
2024-12-27 23:24:17 +0900

    refactor(zsh): remove merge.conflictStyle configuration

--- conf/.zsh/70_other.zsh
+++ conf/.zsh/70_other.zsh
@@ -2,7 +2,7 @@
 if command -v delta &>/dev/null; then
 	git config --global core.pager delta
 	git config --global interactive.diffFilter 'delta --color-only'
-	git config --global merge.conflictStyle zdiff3
+	git config --global --unset merge.conflictStyle
 	git config --global delta.navigate true
 	git config --global delta.line-numbers true
 	git config --global delta.side-by-side true
